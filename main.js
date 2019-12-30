ba8490c1693fc09c3b139154ee430f76cc42f102
Nokocchi <59347338+Nokocchi@users.noreply.github.com>
2019-12-30 02:27:27 +0100

    Can I make sprites appear?

--- main.js
+++ main.js
@@ -4,4 +4,17 @@ window.onload = function () {
 
 function init(){
   var listDiv = document.getElementById("pokemonList");
+  var dir = "src/crystal/";
+var fileextension = ".png";
+$.ajax({
+    //This will retrieve the contents of the folder if the folder is configured as 'browsable'
+    url: dir,
+    success: function (data) {
+        //List all .png file names in the page
+        $(data).find("a:contains(" + fileextension + ")").each(function () {
+            var filename = this.href.replace(window.location.host, "").replace("http://", "");
+            $("body").append("<img src='" + dir + filename + "'>");
+        });
+    }
+});
 }
